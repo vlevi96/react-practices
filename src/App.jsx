2911366d50433ab84bbce42c9590ace7c4feb5b7
vlevi96 <vlevi9609@gmail.com>
2022-01-07 14:22:07 +0100

    Pénteki óra

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonWithClass from './ButtonWithClass';
+import ButtonWithHooks from './ButtonWithHooks';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonWithClass />
+      <ButtonWithHooks />
+    </div>
+  );
+}
+
+export default App;
