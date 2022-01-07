db341049541efcffbd89a5aadbc2fa15e6570675
vlevi96 <vlevi9609@gmail.com>
2022-01-07 14:24:58 +0100

    Sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
-import ButtonWithClass from './ButtonWithClass';
-import ButtonWithHooks from './ButtonWithHooks';
+
 
 function App() {
   return (
     <div className="App">
-      <ButtonWithClass />
-      <ButtonWithHooks />
+
     </div>
   );
 }
